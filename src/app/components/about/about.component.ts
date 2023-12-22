cbb29e4d84f73476a90362f89d90d5a5f2c8665d
Vitor Piovezam <vitor@piovezam.ru>
2023-12-22 14:40:31 -0300

    Updates

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -10,13 +10,13 @@ import { Component, OnInit } from '@angular/core';
       <p>
         I like to ride my bike, listen to alternative music, watch documentaries and drink orange juice.</p>
         <p>My passion is development. Optimize processes and shortening paths is an art that programming makes possible.</p>
-        <p>So in recent years I have developed my skills in logic, typescript and cloud computing. Acting as Fullstack Angular/Node, currently as Senior Software Engineer at <a target="_blank"href="https://www.capgemini.com">Capgemini</a>.
+        <p>So in recent years I have developed my skills in logic, typescript and cloud computing. Acting as Fullstack Angular/Node, currently as Senior Software Engineer at <a target="_blank"href="https://www.picpay.com">PicPay</a>.
       </p>
       <p class="icons">
         <ng-content>
         </ng-content>
       </p>
-      <img class="photo" src="/assets/images/perfil.png">
+      <img class="photo" src="/assets/images/perfil.jpg">
     </div>
   </div>
   `,
