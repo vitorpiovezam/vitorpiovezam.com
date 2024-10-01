7214ea487f1d2816bb4a129c929d13c46908c635
Vitor Piovezam <vitorpiovezam@yandex.com>
2024-09-30 22:02:24 -0300

    fixed images

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,11 +7,9 @@ import { Component, OnInit } from '@angular/core';
   <h2>About</h2>
     <div class="text">
       <p>My name is Vitor, I'm {{ myAge }} year's old.</p>
-      <p>
-        I like to ride my bike, listen to alternative music, watch documentaries and drink orange juice.</p>
-        <p>My passion is development. Optimize processes and shortening paths is an art that programming makes possible.</p>
-        <p>So in recent years I have developed my skills in logic, typescript and cloud computing. Acting as Fullstack Angular/Node, currently as Senior Software Engineer at <a target="_blank"href="https://www.picpay.com">PicPay</a>.
-      </p>
+      <p>I like to ride my bike, listen to alternative music, watch documentaries and drink orange juice.</p>
+      <p>My passion is development. Optimize processes and shortening paths is an art that programming makes possible.</p>
+      <p>So in recent years I have developed my skills in logic, typescript and cloud computing. Acting as Fullstack Angular/Node, currently as Senior Software Engineer at <a target="_blank"href="https://www.picpay.com">PicPay</a>.</p>
       <p class="icons">
         <ng-content>
         </ng-content>
