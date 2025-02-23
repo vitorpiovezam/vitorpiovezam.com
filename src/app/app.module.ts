ca61b44233565be2a8e41202e627cc4a5de1bb54
Vitor Piovezam <vitorpiovezam@yandex.com>
2025-02-23 08:11:08 -0300

    fixed images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,44 +7,37 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { LocalStorageService } from './services/local-storage.service';
 import { AboutComponent } from './components/about/about.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { MarkdownModule, MarkdownService } from 'ngx-markdown';
 import { PostListComponent } from './components/post/list.component';
 import { PostViewComponent } from './components/post/view.component';
 import { LoadingComponent } from './components/shared/loading.component';
-@NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    PostListComponent,
-    PostViewComponent,
-    LoadingComponent
-  ],
-  imports: [
-    FontAwesomeModule,
-    BrowserModule,
-    HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    RouterModule.forRoot([
-      { path: 'post/:slug', component: PostViewComponent },
+@NgModule({ declarations: [
+        AppComponent,
+        AboutComponent,
+        PostListComponent,
+        PostViewComponent,
+        LoadingComponent
     ],
-    {
-    scrollPositionRestoration: 'enabled'
-}),
-    MarkdownModule.forRoot({
-      sanitize: SecurityContext.NONE
-    })
-  ],
-  exports: [
-    RouterModule
-  ],
-  providers: [
-    LocalStorageService,
-    MarkdownService,
-    PostService,
-  ],
-  bootstrap: [AppComponent]
-})
+    exports: [
+        RouterModule
+    ],
+    bootstrap: [AppComponent], imports: [FontAwesomeModule,
+        BrowserModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+        RouterModule.forRoot([
+            { path: 'post/:slug', component: PostViewComponent },
+        ], {
+            scrollPositionRestoration: 'enabled'
+        }),
+        MarkdownModule.forRoot({
+            sanitize: SecurityContext.NONE
+        })], providers: [
+        LocalStorageService,
+        MarkdownService,
+        PostService,
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule { }
